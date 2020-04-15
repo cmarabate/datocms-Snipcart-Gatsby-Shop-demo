a18564d3370821b2c9c320c2aaeb0f7e6f3584b7
cmarabate <cmarabate@gmail.com>
2020-04-15 13:50:21 -0400

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,19 +5,6 @@ import Link from 'gatsby-link'
 
 import '../style/index.scss'
 
-
-var paypal = require('paypal-checkout');
-var client = require('braintree-web/client');
-var paypalCheckout = require('braintree-web/paypal-checkout');
-
-paypal.Button.render({
-  braintree: {
-    client: client,
-    paypalCheckout: paypalCheckout
-  },
-  // The rest of your configuration
-}, '#id-of-element-where-paypal-button-will-render');
-
 const Layout = ({ children, site }) => (
   <div>
     <Helmet title="Snipcart + DatoCMS + GatsbyJS Example" />
